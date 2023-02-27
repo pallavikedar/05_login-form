c3f4b53c4bd33d734df58c147ce22ce8b0700cdf
pallavi kedar <kedarpallavi723@gmail.com>
2023-02-27 14:46:41 +0530

    login-form

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Login</h1>
+    <form>
+    <div className='label'><label>Username</label></div>
+    
+    <div className='input'><input type="text" placeholder='username'/></div>
+    
+    
+    <div className='label'><label>Password</label></div>
+    <div className='input'><input type="password" placeholder='password'/></div>
+    <div className='button'><button>Login</button></div>
+    </form>
+    
+      
+    </div>
+  );
+}
+
+export default App;
