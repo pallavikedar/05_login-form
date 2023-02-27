160b23e8d5aef238ece3f1ac99d882c2c4f1397f
pallavi kedar <kedarpallavi723@gmail.com>
2023-02-27 16:57:03 +0530

    Added validation

--- src/App.js
+++ src/App.js
@@ -1,27 +1,57 @@
 import React from "react";
+import {useState} from "react";
 import "./App.css";
+import Validation from "./validation";
 
 function App() {
+  const [values, setValues] = useState({
+    username: "",
+    password: "",
+  });
+  const [errors, setErrors] = useState({});
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors(Validation(values));
+  };
   return (
     <div className="App">
       <h1>Login</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="label">
           <label>Username</label>
         </div>
 
         <div className="input">
-          <input type="text" placeholder="username" />
+          <input
+            type="text"
+            value={values.username}
+            name="username"
+            placeholder="username"
+            onChange={handleChange}
+          />
         </div>
+        {errors.username && <p style={{ color: "red" }}>{errors.username}</p>}
 
         <div className="label">
           <label>Password</label>
         </div>
         <div className="input">
-          <input type="password" placeholder="password" />
+          <input
+            type="password"
+            value={values.password}
+            name="password"
+            placeholder="password"
+            onChange={handleChange}
+          />
         </div>
+        {errors.password && (
+          <p style={{ color: "red" }}>{errors.password}</p>
+        )}
         <div className="button">
-          <button>Login</button>
+          <button type="submit">Login</button>
         </div>
       </form>
     </div>
